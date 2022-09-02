3248238ecf42a3b626edeccceb024a3d281f9f67
schaldach <gabimorgado0311@gmail.com>
2022-09-01 22:07:55 -0300

    começando o portfolio...

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div>Portfólio</div>
+            <div>Tecnologias</div>
+            <div>Projetos</div>
+            <div>Contato</div>
+        </div>
+    );
+}
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Navbar from "../components/Navbar"
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Navbar/>
+    <main>
+      <Component {...pageProps} />
+    </main>
+  </>)
 }
 
 export default MyApp
