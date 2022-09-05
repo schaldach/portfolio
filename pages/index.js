e5d04e444c16ab98a41283a4d62f551fead27cf2
schaldach <gabimorgado0311@gmail.com>
2022-09-05 19:08:34 -0300

    home muito foda e começo dos projetos

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 function LandingPage({changePage}) {
   return (
-    <div>
       <div className="pagesection homesection">
-        <div className="curve"></div>
+        <div class="custom-shape-divider-top-1662411529">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          </svg>
+        </div>
         <div className="home">
         <div className="title">Olá, eu sou <span>Gabriel Schaldach</span><br/> e este é o meu portfólio<br/> como <span>desenvolvedor web.</span></div>
         <Link href='/about'>
@@ -16,7 +19,6 @@ function LandingPage({changePage}) {
         </Link>
         </div>
       </div>
-    </div>
   );
 }
 
--- pages/projetos.js
+++ pages/projetos.js
@@ -1,25 +1,57 @@
+import Image from "next/image";
+import NoltLogo from '../public/noltlogo.png'
+import PongLogo from '../public/ponglogo.png'
+import NewtonLogo from '../public/newtonlogo.png'
+import JsLogo from '../public/jslogo.png'
+import HtmlLogo from '../public/htmllogo.png'
+import CssLogo from '../public/csslogo.png'
+import NextLogo from '../public/nextlogo.png'
+import ReactLogo from '../public/reactlogo.png'
+import P5Logo from '../public/p5logo.png'
+import SupabaseLogo from '../public/supabaselogo.png'
+
 function Projetos() {
     return (
-        <div>
-            <div className="pagesection">
-                <div className="showprojects">
-                    <div>
-                        <div className="showtitle">Nolt</div>
-                        <div className="showicons">
-                            <a href="https://github.com/schaldach/nolt" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
-                            <a href="https://nolt-opal.vercel.app/" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" /></svg></a>
-                        </div>
+    <div className="pagesection projectsection">
+        <div class="custom-shape-divider-top-1662411529">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <div className="showprojects">
+            <div>
+                <div className="showtitle">Nolt
+                    <div className="showicons">
+                        <a href="https://github.com/schaldach/nolt" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
+                        <a href="https://nolt-opal.vercel.app/" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" /></svg></a>
                     </div>
-                    <div>
-                        <div className="showtitle">Pong do Morgs</div>
-                        <div className="showicons">
-                            <a href="https://github.com/schaldach/nolt" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
-                            <a href="https://nolt-opal.vercel.app/" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" /></svg></a>
-                        </div>
+                </div>
+                <div className="showdescription">O Nolt foi um site criado para organizar diversos tipos de anotações, entre eles notas, listas, links e imagens. Ele possui uma database na Supabase, e qualquer um pode criar seu usuário e começar a usá-lo para organizar as diversas coisas do dia-a-dia, de diversas maneiras diferentes.</div>
+                <div className="techused">
+                    <Image src={HtmlLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={CssLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={JsLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={ReactLogo} width='38' height='35' layout='fixed'/>
+                    <Image src={NextLogo} width='70' height='35' layout='fixed'/>
+                    <Image src={SupabaseLogo} width='35' height='35' layout='fixed'/>
+                </div>
+            </div>
+            <div>
+                <div className="showtitle">Pong do Morgs
+                    <div className="showicons">
+                        <a href="https://github.com/schaldach/nolt" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
+                        <a href="https://nolt-opal.vercel.app/" target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" /></svg></a>
                     </div>
+                </div>
+                <div className="techused">
+                    <Image src={HtmlLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={CssLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={JsLogo} width='35' height='35' layout='fixed'/>
+                    <Image src={P5Logo} width='35' height='35' layout='fixed'/>
                 </div>
             </div>
         </div>
+    </div>
     );
 }
 
