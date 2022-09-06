c4964045e0feeb77dc1d574b69732ee3f12714a9
schaldach <gabimorgado0311@gmail.com>
2022-09-05 21:29:39 -0300

    adicionando os projetos

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar"
 import React, {useState} from "react"
+import Head from 'next/head'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
   const [currentPage, changePage] = useState('home')
   return (
   <>
+    <Head>
+      <title>Gabriel Schaldach Morgado</title>
+    </Head>
     <Navbar currentPage={currentPage} changePage={changePage}/>
     <main>
       <Component changePage={changePage} {...pageProps} />
