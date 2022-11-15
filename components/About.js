4cab078db28ff9fe6800f75ddcd60e5b16ded66a
schaldach <gabimorgado0311@gmail.com>
2022-11-15 20:30:58 -0300

    hover na navbar

--- components/About.js
+++ components/About.js
@@ -7,7 +7,7 @@ function About() {
       <a id='about'></a>
       <div className="about">
         <div>
-          <div className="description">&#128073; Me chamo Gabriel Schaldach Morgado, tenho 17 anos e estou no terceiro ano do Ensino Médio. </div>
+          <div className="description">&#128073; Me chamo Gabriel Schaldach Morgado, tenho 18 anos e estou no terceiro ano do Ensino Médio. </div>
           <div className="description">&#128073; A linguagem que eu sou mais experiente e confortável é JavaScript, principalmente no <span>Frontend</span> de sites, usando Next/React como frameworks.</div>
           <div className="description">&#128073; Tenho um pouco de experiência em databases Postgres, no <span>Backend</span>, e Python, no <span>processamento de dados e imagens</span>.</div>
         </div>
