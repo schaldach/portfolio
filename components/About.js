2cf17f9e70d16f4bee6b173a0c116ccfc7a4f3fd
schaldach <gabimorgado0311@gmail.com>
2023-04-23 15:31:44 -0300

    mudanças pequenas

--- components/About.js
+++ components/About.js
@@ -5,12 +5,12 @@ const content = {
   'pt-BR': [
     'Me chamo Gabriel Schaldach Morgado, tenho 18 anos e atualmente estou cursando bacharelado em Ciência da Computação na UNIVALI Itajaí, e trabalhando como desenvolvedor Web Full Stack.',
     <> A linguagem que eu sou mais experiente e confortável é JavaScript, principalmente no <span>Frontend</span>, usando Next e React como frameworks para fazer sites interativos e responsivos.</>,
-    <> Tenho experiência também no <span>Backend</span>, onde já aprendi alguns conceitos fundamentais sobre databases, utilizando a plataformas Supabase e Firebase para bases de dados NoSQL, e estou aprendendo Prisma para bases de dados PostgreSQL.</>
+    <> Tenho experiência também no <span>Backend</span>, onde já aprendi alguns conceitos fundamentais sobre databases, utilizando a plataforma Supabase para bases de dados PostgreSQL, e já utilizei Firebase, e estou aprendendo MongoDB com Prisma, para bases de dados NoSQL.</>
   ],
   'en-US': [
     'My name is Gabriel Schaldach Morgado, I am 18 years old and currently I am pursuing a Bachelors degree in Computer Science at UNIVALI Itajaí, and working as a Full Stack Web developer.',
     <> The language I am most experienced and comfortable in is Javascript, especially in the <span>Frontend</span>, using Next and React as frameworks to make interactive and responsive websites.</>,
-    <>I also have experience on the <span>Backend</span>, where I learned fundamental concepts about databases, using the Supabase and Firebase platforms to work with NoSQL databases, and I am currently learning Prisma to work with PostgreSQL databases.</>
+    <> I also have experience on the <span>Backend</span>, where I learned fundamental concepts about databases, using the Supabase platform to work with PostgreSQL databases, and I have already used Firebase, and I am learning MongoDB with Prisma, to work with NoSQL databases.</>
   ]
 }
 
